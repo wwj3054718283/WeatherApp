ea941523acfaf07cca5d95b1bc9ccef08f87a4ef
coderwwj <3054718283@qq.com>
2020-11-21 09:41:57 +0800

    feat：app界面初始化

---
+++ app/src/main/java/com/example/weatherapp/ChooseArea.java
@@ -0,0 +1,14 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_area);
+    }
+}
