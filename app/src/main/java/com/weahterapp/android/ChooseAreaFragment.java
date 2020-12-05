49e5c59dc3a337b75be6f08a4dfeca3d4b3fd7e9
coderwwj <3054718283@qq.com>
2020-12-05 08:58:31 +0800

    feat：遍历省市县功能

--- app/src/main/java/com/weahterapp/android/ChooseAreaFragment.java
+++ app/src/main/java/com/weahterapp/android/ChooseAreaFragment.java
@@ -2,6 +2,9 @@ package com.weahterapp.android;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,16 +15,12 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import com.example.weatherapp.R;
 import com.weahterapp.android.db.City;
 import com.weahterapp.android.db.County;
 import com.weahterapp.android.db.Province;
 import com.weahterapp.android.util.HttpUtil;
 import com.weahterapp.android.util.Utility;
+import com.weatherapp.android.R;
 
 import org.litepal.crud.DataSupport;
 
@@ -33,6 +32,11 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ChooseAreaFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class ChooseAreaFragment extends Fragment {
     public static final int LEVEL_PROVINCE = 0;
     public static final int LEVEL_CITY = 1;
@@ -67,9 +71,50 @@ public class ChooseAreaFragment extends Fragment {
      * 当前选中的级别
      */
     private int currentLevel;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ChooseAreaFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ChooseAreaFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ChooseAreaFragment newInstance(String param1, String param2) {
+        ChooseAreaFragment fragment = new ChooseAreaFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.choose_area,container,false);
         titleText = view.findViewById(R.id.title_text);
         backButton = view.findViewById(R.id.back_button);
@@ -106,10 +151,6 @@ public class ChooseAreaFragment extends Fragment {
         });
         queryProvinces();
     }
-
-    /**
-     * 查询全国所有的省，优先从数据库查询，如果没有查询到再去服务器查询
-     */
     private void queryProvinces(){
         titleText.setText("中国");
         backButton.setVisibility(View.GONE);
@@ -125,12 +166,9 @@ public class ChooseAreaFragment extends Fragment {
         }else{
             String address = "http:guolin.tech/api/china";
             queryFromServer(address,"province");
+            System.out.println(address);
         }
     }
-
-    /**
-     * 查询选中省内所有的市，优先从数据库查询，如果没有查询到再去服务器查询
-     */
     private void queryCities(){
         titleText.setText(selectedProvince.getProvinceName());
         backButton.setVisibility(View.VISIBLE);
@@ -147,12 +185,9 @@ public class ChooseAreaFragment extends Fragment {
             int provinceCode = selectedProvince.getId();
             String address = "http:guolin.tech/api/china/" + provinceCode;
             queryFromServer(address,"city");
+
         }
     }
-
-    /**
-     * 查询选中市内的县，优先从数据库查询，如果没有查询到再去服务器查询
-     */
     private void queryCounties(){
         titleText.setText(selectedCity.getCityName());
         backButton.setVisibility(View.VISIBLE);
@@ -172,24 +207,9 @@ public class ChooseAreaFragment extends Fragment {
             queryFromServer(address,"county");
         }
     }
-
-    /**
-     * 根据传入的地址和类型从服务器上查询省市县数据
-     */
-    private void queryFromServer(String address,final String type){
+    private void queryFromServer(final String address, final String type){
         showProgressDialog();
         HttpUtil.sendOkHtppRequest(address, new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        closeProgressDialog();
-                        Toast.makeText(getContext(),"加载失败",Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 String responseText = response.body().string();
@@ -217,12 +237,21 @@ public class ChooseAreaFragment extends Fragment {
                     });
                 }
             }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(),"加载失败"+address,Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+
         });
     }
-
-    /**
-     * 显示进度对话框
-     */
     private void showProgressDialog(){
         if (progressDialog == null){
             progressDialog = new ProgressDialog(getActivity());
@@ -231,10 +260,6 @@ public class ChooseAreaFragment extends Fragment {
         }
         progressDialog.show();
     }
-
-    /**
-     * 关闭进度对话框
-     */
     private void closeProgressDialog(){
         if (progressDialog != null){
             progressDialog.dismiss();
--- app/src/main/java/com/weahterapp/android/util/HttpUtil.java
+++ app/src/main/java/com/weahterapp/android/util/HttpUtil.java
@@ -1,11 +1,10 @@
 package com.weahterapp.android.util;
 
-import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
 public class HttpUtil {
-    public static void sendOkHtppRequest(String address, Callback callback){
+    public static void sendOkHtppRequest(String address, okhttp3.Callback callback){
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(address).build();
         client.newCall(request).enqueue(callback);
