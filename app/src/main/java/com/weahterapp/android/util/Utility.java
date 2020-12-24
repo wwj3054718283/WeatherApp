afa54245b4df6c2ce2863f11f8aee76df62f3bae
coderwwj <3054718283@qq.com>
2020-12-24 09:45:06 +0800

    feat：预览天气

--- app/src/main/java/com/weahterapp/android/util/Utility.java
+++ app/src/main/java/com/weahterapp/android/util/Utility.java
@@ -2,9 +2,11 @@ package com.weahterapp.android.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.weahterapp.android.db.City;
 import com.weahterapp.android.db.County;
 import com.weahterapp.android.db.Province;
+import com.weahterapp.android.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -78,5 +80,20 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
